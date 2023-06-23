fc30e7f9a1709fa2c0a5d622aef4246927b6638a
specialobyno <specialobyno@yahoo.com>
2023-06-23 12:16:03 -0700

    New Commit:

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string
+ * Return: 0 if successful. Otherwise 1
+ */
+
+int main()
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+}
