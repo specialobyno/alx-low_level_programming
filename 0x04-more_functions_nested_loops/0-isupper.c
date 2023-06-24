858d5d55f6d9bb668c4a511016ef54311f00ccb8
specialobyno <specialobyno@yahoo.com>
2023-06-23 18:01:50 -0700

    New Commit:

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * _isupper - uppercase letters
+ * _isupper - to check uppercase letters
  * @c: the character to check
  * Return: 0 or 1
  */
 int _isupper(int c)
-
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
