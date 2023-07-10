92051a674810eca9412f322ad03e8f81502d2404
specialobyno <specialobyno@yahoo.com>
2023-07-10 14:08:59 -0700

    New Commit:

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints the number of argument passed to it
+* @argc - number of arguments passed to it
+* @argv[] - array containing all command line parameters
+* Return: 0 on success, non-zero otherwise (errno)
+*/
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+    printf("%d\n", argc);
+    return (0);
+}
