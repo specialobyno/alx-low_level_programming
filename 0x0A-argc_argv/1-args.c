b157379b9d62a0f9895a754cc819b4c8af92b71b
specialobyno <specialobyno@yahoo.com>
2023-07-10 14:13:20 -0700

    New Commit:

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,7 +8,7 @@
 * Return: 0 on success, non-zero otherwise (errno)
 */
 
-int main(int argc, char **argv __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
     printf("%d\n", argc - 1);
     return (0);
