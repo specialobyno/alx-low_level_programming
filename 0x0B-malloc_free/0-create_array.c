a790533984a5c804e0b9d950377a16077341b9b1
specialobyno <specialobyno@yahoo.com>
2023-07-11 17:23:58 -0700

    New Commit:

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * creat_array - creates an array of chars,
+ * and initializes it with a specific char
+ * @size -
+ * @c - 
+ * Return: NULL if size is 0, or pointer to the array
+*/
+
+char *create_array(unsigned int size, char c)
+{
+        char *arr;
+		unsigned int i;
+
+		if (size == 0)
+		{
+			return (NULL);
+		}
+
+		arr = malloc(sizeof(char) * size);
+
+		if (arr == NULL)
+		{
+
+			return (NULL);
+		}
+
+		for (i = 0; i < size; i++)
+		{
+			arr[i] = c;
+		}
+
+		return (arr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+New readme for malloc
