3b58920f3a853e8127ddf2293cb8064a838e1657
specialobyno <specialobyno@yahoo.com>
2023-07-18 00:56:16 -0700

    New Commit:

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it compiles from
+ * followed by a new line
+ * Return: 0 if successful
+*/
+
+int main()
+{
+    printf("%s", __FILE__);
+    return (0);
+}
+
