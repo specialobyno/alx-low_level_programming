e38111835b078ccbfd6a90e88a3a17c1d1a24317
specialobyno <specialobyno@yahoo.com>
2023-07-20 04:34:54 -0700

    New Commit:

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,11 +14,13 @@ int sum_them_all(const unsigned int n, ...)
 {
     if(n == 0)
         return (0);
-    va_list nums;
-    va_start(nums, n);
-    int i, sum = 0;
-    for (i = 0; i <= n; i++)
-        sum += va_arg(nums, int);
-    va_end(nums);
+    va_list op;
+    va_start(op, n);
+    int i;
+    int sum = 0;
+    for (i = 0; i < n; i++)
+        sum += va_arg(op, int);
+    va_end(op);
     return (sum);
 }
+
