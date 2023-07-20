57719d33a548d0d1c1587928aca0315e28bbe4bc
specialobyno <specialobyno@yahoo.com>
2023-07-20 05:09:24 -0700

    New Commit:

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -6,25 +6,26 @@
  * @separator: the separator to put inbetween numbers
  * @n: the count of integers passed
 */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
     unsigned int i;
     va_list op;
     va_start(op, n);
     if (separator == NULL)
+    {
+        for (i = 0; i < n; ++i)
+            printf("%d", va_arg(op, int));
+    }
+    else 
+    {
+        for (i = 0; i < n; ++i)
         {
-            for (i = 0; i < n; ++i)
-                printf("%d", va_arg(op, int));
+            printf("%d", va_arg(op, int));
+            if(i != n - 1)
+                printf("%s ", separator);
         }
-        else 
-        {
-            for (i = 0; i < n; ++i)
-                {
-                    printf("%d", va_arg(op, int));
-                    if(i != n - 1)
-                        printf("%s ", separator);
-                }
-        }
+    }
     printf("\n");
     va_end(op);
 }
